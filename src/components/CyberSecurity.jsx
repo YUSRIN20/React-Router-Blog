b8cc4826d6630314570b31c1482bf2c1bc1bca1d
YUSRIN20 <massyusrin123@gmail.com>
2024-02-16 03:06:40 +0530

    added Breakpoints for all components

--- src/components/CyberSecurity.jsx
+++ src/components/CyberSecurity.jsx
@@ -6,7 +6,7 @@ const CyberSecurity = ({ data }) => {
     return (
         <div>
             <div class="container px-4 px-lg-5 mt-5">
-                <div class="row gx-4 gx-lg-4 row-cols-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-3 justify-content-center">
+                <div class="row gx-4 gx-lg-4 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 justify-content-center">
                     {CyberSecurityData.map((item, index) => {
                         return (
                             <>
